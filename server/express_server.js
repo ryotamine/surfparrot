1a456c9d4ff57a350d1f66a9b148d4f96513045f
Ryota Mine <ryotamine@gmail.com>
2019-04-26 22:18:45 +0000

    Add concurrently as a dependency

--- server/express_server.js
+++ server/express_server.js
@@ -1,6 +1,6 @@
 const express       = require("express");
 const app           = express();
-const PORT          = 3000
+const PORT          = 5000
 // const SpotifyWebApi = require("spotify-web-api-node");
 
 // Boot server
