c241fefb3149301d908b7c93e5fa34bba56ce9f6
Ryota Mine <ryotamine@gmail.com>
2019-04-28 22:30:48 +0000

    Remove extra react render files

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import Home from './home';
-import Login from './login';
-import Register from './register';
-import Contact from './contact';
 import Scrape from './external-show-listings/scrape';
 import SpotifyPlayer from './SpotifyPlayer';
 
@@ -10,18 +6,11 @@ import SpotifyPlayer from './SpotifyPlayer';
 class App extends Component {
   render() {
     return (
-      
       <div>
-        <Home />
-        <Login />
-        <Register />
-        <Contact />
         <Scrape />
         {/* this.state.artistid */}
         <SpotifyPlayer artistid="0Z8fvErw8r7KKFjYAWDd0a"/>
       </div>
-      
-
     );
   }
 }
