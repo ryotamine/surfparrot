f08314185bc4e5eb0f28981bd8837656455854e3
Ryota Mine <ryotamine@gmail.com>
2019-04-29 22:28:52 +0000

    Convert login popup from HTML to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Home from './home';
+import Login from './login';
 import Scrape from './external-show-listings/scrape';
 import SpotifyPlayer from './SpotifyPlayer';
 
@@ -7,6 +9,8 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Home />
+        <Login />
         <Scrape />
         {/* this.state.artistid */}
         <SpotifyPlayer artistid="0Z8fvErw8r7KKFjYAWDd0a"/>
--- client/src/SpotifyPlayer.js
+++ client/src/SpotifyPlayer.js
@@ -5,7 +5,7 @@ class SpotifyPlayer extends Component {
 
 
     render() {
-        return <iframe src={`https://open.spotify.com/embed/artist/${this.props.artistid}`} width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        return <iframe src={`https://open.spotify.com/embed/artist/${this.props.artistid}`} width="300" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 
     }
 
