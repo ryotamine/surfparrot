70e9a4d3bc14155595666b0fc7a44a8e08426c3f
esplett <esthersplett@gmail.com>
2019-04-30 17:26:13 +0000

    debugging errors

--- server/getSpotifyToken.js
+++ server/getSpotifyToken.js
@@ -1,48 +1,52 @@
 const request = require('request')
+const base64url = require('base64url');
 
 
 const getSpotifyToken = ({
-    'cf87031075d34057a785f63be7aac4e7',
-    'e318cad2d4d04e6c9e16df1b01372350'
+    clientId,
+    clientSecret
   }, done) =>
   request({
       method: 'POST',
-      url: 'https://accounts.spotify.com/api/token',
+      uri: 'https://accounts.spotify.com/api/token',
       form: {
         grant_type: 'client_credentials',
       },
       headers: {
-        Authorization: 'Basic ' + encodeInBase64('cf87031075d34057a785f63be7aac4e7' + ':' + 'e318cad2d4d04e6c9e16df1b01372350'),
+        'Content-type': 'application/x-www-form-urlencoded',
+        Authorization: 'Basic ' + base64url(clientId + ':' + clientSecret),
       },
       json: true,
     },
-    
+
     (error, response, body) => {
-      console.log(body)
-    //   if (error) {
-    //     done(error)
-    //   } else if (response.statusCode < 200 || response.statusCode > 299) {
-    //     done(new Error('Error ' + response.statusCode + ' from Spotify API'))
-    //   } else if (body && body.error) {
-    //     done(
-    //       new Error(
-    //         body.error + ' ' + body.error_description + ' from Spotify API'
-    //       )
-    //     )
-    //   } else if (!body) {
-    //     done(new Error('No token returned by Spotify API.'))
-    //   } else {
-    //     done(null, body.access_token)
-    //   }
+      console.log("body~~~~~~~~~~~~~~~~~", body)
+      console.log("error~~~~~~~~~~~~~~~~~~~", error)
+      console.log("response~~~~~~~~~~~~~~~~~~~~~", response)
+      if (error) {
+        done(error)
+      } else if (response.statusCode < 200 || response.statusCode > 299) {
+        done(new Error('Error ' + response.statusCode + ' from Spotify API'))
+      } else if (body && body.error) {
+        done(
+          new Error(
+            body.error + ' ' + body.error_description + ' from Spotify API'
+          )
+        )
+      } else if (!body) {
+        done(new Error('No token returned by Spotify API.'))
+      } else {
+        done(null, body.access_token)
+      }
     }
   )
 
-function encodeInBase64(s) {
-  if (typeof window === 'object' && window.btoa) {
-    return window.btoa(s)
-  } else {
-    return new Buffer(s).toString('base64')
-  }
-}
+// function encodeInBase64(s) {
+//   if (typeof window === 'object' && window.btoa) {
+//     return window.btoa(s)
+//   } else {
+//     return new Buffer(s).toString('base64')
+//   }
+// }
 
 module.exports = getSpotifyToken
