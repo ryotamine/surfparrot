eb0c987ea695f86ffccb70d299d2dc9549073215
Ryota Mine <ryotamine@gmail.com>
2019-05-05 19:20:49 +0000

    Enable delete event button for both artist and user

--- client/src/user_content.js
+++ client/src/user_content.js
@@ -2,16 +2,43 @@ import React, { Component } from 'react';
 
 // User content class
 class UserContent extends Component {
+  // User content constructor
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      data: [{eventName: ""}]
+    };
+
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  // Delete event helper function
+  handleDelete(event) {
+    const data = this.state.data.filter(i => i.eventName !== event.eventName)
+    this.setState({data});
+  }
+
+  // Render user content
   render() {
+    const events = this.state.data.map((event) => {
+      return (
+        <div key={event.eventName}>
+          <li className="event-info">
+            <div className="align-event-option">
+              <button className="delete-event" onClick={this.handleDelete.bind(this, event)}>
+                <i class="far fa-trash-alt"></i>
+              </button>
+            </div>
+          </li>
+        </div>
+      );
+    });
+
     return (
       <div>
         <h1 className="user-upcoming-events">Upcoming Events</h1>
-
-        <li className="event-info">
-          <div className="align-event-option">
-            <button className="delete-event"><i class="far fa-trash-alt"></i></button>
-          </div>
-        </li>
+        {events}
       </div>
     );
   }
