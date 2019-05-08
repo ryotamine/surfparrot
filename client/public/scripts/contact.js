6ee388141b1a2be1ce5719acb5b240ef74bc2f16
Ryota Mine <ryotamine@gmail.com>
2019-05-08 22:06:27 +0000

    Format code in public folder

--- client/public/scripts/contact.js
+++ client/public/scripts/contact.js
@@ -1,9 +1,9 @@
 // Get the modal
-var modal = document.getElementById('id03');
+const modal = document.getElementById('id03');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
